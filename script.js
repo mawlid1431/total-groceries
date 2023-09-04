adfe73a14b4d4f4f70c04b5ad8e36e8a33fc7e9e
malit mohamut <60866464+mawlid1431@users.noreply.github.com>
2023-09-04 15:02:24 +0300

    grocesry exercise

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-// Good Luck 💪🏾
+// Good Luck 💪🏾   
+
+
+
+function getTotalPrice(groceries) {
+  let total = 0;
+
+  for (const item of groceries) {
+    total += item.quantity * item.price;
+  }
+
+  return total;
+}
+
+// Example usages:
+const groceries1 = [
+  { product: "Milk", quantity: 1, price: 1.50 }
+];
+console.log(getTotalPrice(groceries1)); // Output: 1.5
+
+const groceries2 = [
+  { product: "Milk", quantity: 1, price: 1.50 },
+  { product: "Cereals", quantity: 1, price: 2.50 }
+];
+console.log(getTotalPrice(groceries2)); // Output: 4
+
+const groceries3 = [
+  { product: "Milk", quantity: 3, price: 1.50 }
+];
+console.log(getTotalPrice(groceries3)); // Output: 4.5
+
